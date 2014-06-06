b00c9f3dee5008135227fb8252fa67134e5cd187
System Administrator <root@iMac-di-iMac.local>
2014-06-06 17:02:31 +0200

    mockup set result and update points (need refactoring)

--- server/server.js
+++ server/server.js
@@ -29,6 +29,7 @@ Meteor.startup(function () {
 
 Accounts.onCreateUser(function(options, user) {
   user.points = 0;
+  user.admin = false;
   return user;
 });
 
@@ -86,6 +87,66 @@ Meteor.methods({
     chat: function (message) {
         var user = Meteor.user();
         Messages.insert({message: message, time: new Date(), username: user.username});
+    }
+});
+
+Meteor.methods({
+    update: function () {
+
+        var usersPoints = {};
+        var rounds = Rounds.find().fetch();
+        for (var i = 0; i < rounds.length; i++) {
+            var round = rounds[i];
+            for (var j = 0; j < round["games"].length; j++) {
+                var game = round["games"][j];
+                var score1 = parseInt(game["score1"]);
+                var score2 = parseInt(game["score2"]);
+                var bets = Bets.find({game_id: game["_id"]}).fetch();
+                for (var k = 0; k < bets.length; k++) {
+                    var bet = bets[k];
+                    var betScore1 = parseInt(bet["team1"]);
+                    var betScore2 = parseInt(bet["team2"]);
+                    var bonus = bet["bonus"];
+                    var points = 0;
+                    if (score1==betScore1 && score2==betScore2) {
+                        points = 3;
+                    } else if (score1>score2 && betScore1>betScore2) {
+                        points = 1;
+                    } else if (score1<score2 && betScore1<betScore2) {
+                        points = 1;
+                    } else if (score1==score2 && betScore1==betScore2) {
+                        points = 1;
+                    } 
+                    if (bonus) {
+                        points *= 2;
+                    };
+                    usersPoints[bet["user_id"]] = (usersPoints[bet["user_id"]]===undefined) ? points : usersPoints[bet["user_id"]] + points;
+                };
+            };
+        };
+
+        
+        for (var userId in usersPoints) {
+            Meteor.users.update({_id: userId}, {$set: {points: usersPoints[userId]}});
+        }
+
+    }
+});
+
+Meteor.methods({
+    gameResult: function(gameId, score1, score2) {
+        var round = Rounds.findOne({
+            games: {$elemMatch : {_id: gameId}}
+        });
+        for (var i = 0; i < round["games"].length; i++) {
+            var game = round["games"][i];
+            if (game["_id"] == gameId) {
+                var set = {};
+                set['games.'+i+'.score1'] = parseInt(score1);
+                set['games.'+i+'.score2'] = parseInt(score2);
+                Rounds.update({_id: round["_id"]}, {$set: set});
+            };
+        };
     }
 });
 
